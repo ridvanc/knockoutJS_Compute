900f4a27fcd2bc981475e5f00dae83ff9ee9fe9c
ridvanc <ridvancetin@outlook.com>
2013-11-03 20:54:52 +0200

    delete file

--- Scripts/helloknockout.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="jquery-2.0.3.js" />
-/// <reference path="knockout-3.0.0.js" />
-var mainViewModel = function () {
-    var self = this;
-    self.firstText = ko.observable("Hello Knockout");
-}
-$(document).ready(function () {
-    ko.applyBindings(mainViewModel);
-});
